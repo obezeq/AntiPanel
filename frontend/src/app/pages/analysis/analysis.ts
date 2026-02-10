c5e055384efc224018e6b1d3e57c4fbe9084522e
= <eortbra2812@g.educaand.es>
2026-02-10 09:39:26 +0100

    (feat) adding analysis header section and content section

--- frontend/src/app/pages/analysis/analysis.ts
+++ frontend/src/app/pages/analysis/analysis.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, ChangeDetectionStrategy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Header } from '../../components/layout/header/header';
+import { Footer } from '../../components/layout/footer/footer';
+import { AnalysisHeaderSection } from './sections/analysis-header-section/analysis-header-section' ;
+import { AnalysisContentSection } from './sections/analysis-content-section/analysis-content-section';
 
 @Component({
   selector: 'app-analysis',
-  imports: [],
+  imports: [Header, Footer, AnalysisHeaderSection, AnalysisContentSection],
   templateUrl: './analysis.html',
   styleUrl: './analysis.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Analysis {
-
+  /*
+  protected readonly headerVariant = computed<HeaderVariant>(() =>
+    this.authService.isAuthenticated() ? 'loggedIn' : 'home'
+  );
+  */
 }
