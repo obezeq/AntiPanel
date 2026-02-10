0d2ef9385df9e4261313caa41d055c8e58cb5d00
= <eortbra2812@g.educaand.es>
2026-02-10 11:16:50 +0100

    (feat) adding responsive to the analysis cards, 1, 2, 3, columns depending on the viewport

---
+++ frontend/src/app/components/shared/analysis-card/analysis-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis-card',
+  imports: [],
+  templateUrl: './analysis-card.html',
+  styleUrl: './analysis-card.scss',
+})
+export class AnalysisCard {
+
+}
