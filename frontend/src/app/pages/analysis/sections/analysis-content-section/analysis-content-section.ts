1af4dea58897c3e193e55b647926fc90a90c15bd
= <eortbra2812@g.educaand.es>
2026-02-10 11:10:51 +0100

    (feat) analysis content section done with responsive

--- frontend/src/app/pages/analysis/sections/analysis-content-section/analysis-content-section.ts
+++ frontend/src/app/pages/analysis/sections/analysis-content-section/analysis-content-section.ts
@@ -1,15 +1,23 @@
-import { Component, ChangeDetectionStrategy, computed, input, output } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal, computed, input, output } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
 // import type { UsersAnalyisisResponse } from '../../../../core/services/analysis.service';
 
 @Component({
   selector: 'app-analysis-content-section',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './analysis-content-section.html',
   styleUrl: './analysis-content-section.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AnalysisContentSection {
+  
 
+  /** Stats data */
+  // readonly stats = input.required<StatsCardData>();
+
+  readonly money = signal("369.33");
+  readonly orders = signal("33");
+  readonly users = signal("69");
 
   // readonly analysis = input.required<UsersAnalyisisResponse>();
 
